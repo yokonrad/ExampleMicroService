4117dce5029729f8bab10c4d7d2f25f9b32389c1
kopuchlik <konrad.opuchlik@gomez.pl>
2024-08-08 00:34:28 +0200

    Added exception filter

---
+++ Services/Shared/Filters/ExceptionFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Shared.Exceptions;
+
+namespace Shared.Filters
+{
+    public class ExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            switch (context.Exception)
+            {
+                case DatabaseException:
+                case MapperException:
+                    context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+                    break;
+
+                case NotFoundException:
+                    context.Result = new NotFoundResult();
+                    break;
+            }
+        }
+    }
+}
