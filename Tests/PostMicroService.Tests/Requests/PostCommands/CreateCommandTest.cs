fabb1534823a3d4029c04fe29a3728e0e97a0659
kopuchlik <konrad.opuchlik@gomez.pl>
2024-11-23 13:05:02 +0100

    Added tests for PostRequest

---
+++ Tests/PostMicroService.Tests/Requests/PostCommands/CreateCommandTest.cs
@@ -0,0 +1,76 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using MassTransit;
+using MassTransit.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using PostMicroService.Requests;
+using Shared.Exceptions;
+using Shared.Requests;
+using Shared.Responds;
+
+namespace PostMicroService.Tests.Requests.PostCommands
+{
+    internal class CreateCommandTest
+    {
+        private Mock<IRequestClient<DeletePostRequest>> deletePostRequestClientMock;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            deletePostRequestClientMock = new Mock<IRequestClient<DeletePostRequest>>();
+        }
+
+        [Test]
+        public async Task Create_Throws_InvalidResponseException()
+        {
+            // Arrange
+            var createPostRequest = new Fixture().Build<CreatePostRequest>().Create();
+            var postNotCreatedRespond = new Fixture().Build<PostNotCreatedRespond>().With(x => x.CorrelationId, createPostRequest.CorrelationId).With(x => x.Id, createPostRequest.Id).Create();
+
+            await using var provider = new ServiceCollection()
+                .AddMassTransitTestHarness(c => c.AddHandler<CreatePostRequest>(async c => await c.RespondAsync(postNotCreatedRespond)))
+                .BuildServiceProvider(true);
+
+            var testHarness = provider.GetRequiredService<ITestHarness>();
+
+            await testHarness.Start();
+
+            var createPostRequestClient = testHarness.GetRequestClient<CreatePostRequest>();
+
+            var postRequest = new PostRequest(createPostRequestClient, deletePostRequestClientMock.Object);
+
+            // Act
+            var act = () => postRequest.Create(createPostRequest);
+
+            // Assert
+            await act.Should().ThrowAsync<InvalidResponseException>();
+        }
+
+        [Test]
+        public async Task Create_Returns_NotNullObject()
+        {
+            // Arrange
+            var createPostRequest = new Fixture().Build<CreatePostRequest>().Create();
+            var postCreatedRespond = new Fixture().Build<PostCreatedRespond>().With(x => x.CorrelationId, createPostRequest.CorrelationId).With(x => x.Id, createPostRequest.Id).Create();
+
+            await using var provider = new ServiceCollection()
+                .AddMassTransitTestHarness(c => c.AddHandler<CreatePostRequest>(async c => await c.RespondAsync(postCreatedRespond)))
+                .BuildServiceProvider(true);
+
+            var testHarness = provider.GetRequiredService<ITestHarness>();
+
+            await testHarness.Start();
+
+            var createPostRequestClient = testHarness.GetRequestClient<CreatePostRequest>();
+
+            var postRequest = new PostRequest(createPostRequestClient, deletePostRequestClientMock.Object);
+
+            // Act
+            var act = await postRequest.Create(createPostRequest);
+
+            // Assert
+            act.Should().BeOfType<PostCreatedRespond>().And.NotBeNull().And.BeEquivalentTo(postCreatedRespond);
+        }
+    }
+}
---
+++ Tests/PostMicroService.Tests/Requests/PostCommands/DeleteCommandTest.cs
@@ -0,0 +1,76 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using MassTransit;
+using MassTransit.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using PostMicroService.Requests;
+using Shared.Exceptions;
+using Shared.Requests;
+using Shared.Responds;
+
+namespace PostMicroService.Tests.Requests.PostCommands
+{
+    internal class DeleteCommandTest
+    {
+        private Mock<IRequestClient<CreatePostRequest>> createPostRequestClientMock;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            createPostRequestClientMock = new Mock<IRequestClient<CreatePostRequest>>();
+        }
+
+        [Test]
+        public async Task Delete_Throws_InvalidResponseException()
+        {
+            // Arrange
+            var deletePostRequest = new Fixture().Build<DeletePostRequest>().Create();
+            var postNotDeletedRespond = new Fixture().Build<PostNotDeletedRespond>().With(x => x.CorrelationId, deletePostRequest.CorrelationId).With(x => x.Id, deletePostRequest.Id).Create();
+
+            await using var provider = new ServiceCollection()
+                .AddMassTransitTestHarness(c => c.AddHandler<DeletePostRequest>(async c => await c.RespondAsync(postNotDeletedRespond)))
+                .BuildServiceProvider(true);
+
+            var testHarness = provider.GetRequiredService<ITestHarness>();
+
+            await testHarness.Start();
+
+            var deletePostRequestClient = testHarness.GetRequestClient<DeletePostRequest>();
+
+            var postRequest = new PostRequest(createPostRequestClientMock.Object, deletePostRequestClient);
+
+            // Act
+            var act = () => postRequest.Delete(deletePostRequest);
+
+            // Assert
+            await act.Should().ThrowAsync<InvalidResponseException>();
+        }
+
+        [Test]
+        public async Task Delete_Returns_NotNullObject()
+        {
+            // Arrange
+            var deletePostRequest = new Fixture().Build<DeletePostRequest>().Create();
+            var postDeletedRespond = new Fixture().Build<PostDeletedRespond>().With(x => x.CorrelationId, deletePostRequest.CorrelationId).With(x => x.Id, deletePostRequest.Id).Create();
+
+            await using var provider = new ServiceCollection()
+                .AddMassTransitTestHarness(c => c.AddHandler<DeletePostRequest>(async c => await c.RespondAsync(postDeletedRespond)))
+                .BuildServiceProvider(true);
+
+            var testHarness = provider.GetRequiredService<ITestHarness>();
+
+            await testHarness.Start();
+
+            var deletePostRequestClient = testHarness.GetRequestClient<DeletePostRequest>();
+
+            var postRequest = new PostRequest(createPostRequestClientMock.Object, deletePostRequestClient);
+
+            // Act
+            var act = await postRequest.Delete(deletePostRequest);
+
+            // Assert
+            act.Should().BeOfType<PostDeletedRespond>().And.NotBeNull().And.BeEquivalentTo(postDeletedRespond);
+        }
+    }
+}
